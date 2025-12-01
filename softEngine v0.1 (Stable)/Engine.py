87dfe050e2730eedec9842ba01a2d39ee0570d30
ryanrossthedev <dev.ryanross.ryanstudios@proton.me>
2025-12-01 20:58:33 +1100

    ..

--- softEngine v0.1 (Stable)/Engine.py
+++ softEngine v0.1 (Stable)/Engine.py
@@ -3,7 +3,4 @@ user = input("Enter a username: ")
 print(user)
 print("logged in.")
 
-value_A = int(input("Please enter value A for multiplication: "))
-value_B = int(input("Please enter value B for multiplication: "))
-
-crazyMATHS = (value_A * value_B)
+mode = input("Enter your desired mode (Addition or Multiplication)")
