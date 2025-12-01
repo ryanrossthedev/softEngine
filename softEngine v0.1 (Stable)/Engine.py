181cb73f8da483b9c38bd837a9a7013f007f68c6
ryanrossthedev <dev.ryanross.ryanstudios@proton.me>
2025-12-01 21:19:35 +1100

    v0.2 is out

--- softEngine v0.1 (Stable)/Engine.py
+++ softEngine v0.1 (Stable)/Engine.py
@@ -1,6 +1,24 @@
 user = input("Enter a username: ")
 
-print(user)
-print("logged in.")
+print(user, "logged in.")
 
-mode = input("Enter your desired mode (Addition or Multiplication)")
+while True:
+    mode = input("Enter your desired mode (Addition or Multiplication): ")
+
+    if mode == 'Addition':
+        ValA = int(input("Enter your desired number for Value A: "))
+        ValB = int(input("Enter your desired number for Value B: "))
+
+        CrazyMATHZ = ValA + ValB
+        print("Result:", CrazyMATHZ)
+
+    elif mode == 'Multiplication':
+        ValAM = int(input("Enter your desired number for Value A: "))
+        ValBM = int(input("Enter your desired number for Value B: "))
+
+        CrazyMATHZm = ValAM * ValBM
+        print("Result:", CrazyMATHZm)
+
+    else:
+        print("ERROR: Mode not recognised. Input *is* case sensitive. Ensure correct spelling.")
+
