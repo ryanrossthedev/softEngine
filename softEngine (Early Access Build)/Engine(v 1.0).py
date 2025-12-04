54d60f9c946a3728a55e1b259fbeddbbc2c97463
ryanrossthedev <dev.ryanross.ryanstudios@proton.me>
2025-12-04 12:56:56 +1100

    Update math operations and input modes

--- softEngine (Early Access Build)/Engine(v 1.0).py
+++ softEngine (Early Access Build)/Engine(v 1.0).py
@@ -3,20 +3,36 @@ user = input("Enter a username: ")
 print(user, "logged in.")
 
 while True:
-    mode = input("Enter your desired mode (Big Back or Stickman): ")
+    mode = input("Enter your desired mode (Addition, Subtraction, Multiplication, Division.): ")
 
-    if mode == 'Big Back':
-        Message = input("What is your favourite KFC bundle??")
+    if mode == 'Addition':
+        Message1p1 = int(input("Enter Value A"))
+        Message1p2 = int(input("Enter Value B"))
 
-        CrazyMATHZ = (user, " likes ", Message)
+        CrazyMATHZ = (Message1p1 + Message1p2)
         print("Result:", CrazyMATHZ)
 
-    elif mode == 'Stickman':
-        Message2 = input("What is your favourite small sized dish?")
+    elif mode == 'Subtraction':
+        Message2p1 = int(input("Enter Value A"))
+        Message2p2 = int(input("Enter Value B"))
 
-        CrazyMATHZm = (user, " likes ", Message2)
-        print("Result:", CrazyMATHZm)
+        CrazyMATHZ2 = (Message2p1 - Message2p2)
+        print("Result:", CrazyMATHZ2)
 
+    elif mode == 'Multiplication':
+        Message3p1 = int(input("Enter Value A"))
+        Message3p2 = int(input("Enter Value B"))
+
+        CrazyMATHZ3 = (Message3p1 * Message3p2)
+        print("Result:", CrazyMATHZ3)
+
+    elif mode == 'Division':
+        Message4p1 = int(input("Enter Value A"))
+        Message4p2 = int(input("Enter Value B"))
+
+        CrazyMATHZ4 = (Message4p1 / Message4p2)
+        print("Result:", CrazyMATHZ4)
+        
     else:
         print("ERROR: Mode not recognised. Input *is* case sensitive.")
-
+        
